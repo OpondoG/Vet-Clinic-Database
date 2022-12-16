c91a61a52244cdb3c399cefa83d39786c2009c9b
OpondoG <okonjogilbert2@gmail.com>
2022-12-16 18:33:52 +0300

    Added new tables,change animal table & new queries

--- data.sql
+++ data.sql
@@ -15,4 +15,31 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 			(9,'Boarmon', '2005-6-7', 7, TRUE ,20.4),
 			(10,'Blossom', '1998-10-13', 3, TRUE ,17),
 			(11,'Ditto', '2022-5-14', 4, TRUE ,22);
+
+ -- Day3
+
+INSERT INTO owners (full_name, age)
+	VALUES 	('Sam Smith', 34),
+			('Jennifer Orwell', 19),
+			('Bob ', 45),
+			('Melody Pond', 77),
+			('Dean Winchester', 14),
+			('Jodie Whittaker', 38);
+
+INSERT INTO species (name) 
+	VALUES ('Pokemon'), ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';	
+
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+UPDATE animals set owner_id = 1 WHERE name = 'Agumon';
+
+UPDATE animals set owner_id = 2 WHERE name in ('Gabumon','Pikachu');
+
+UPDATE animals set owner_id = 3 WHERE name in ('Devimon','Plantmon');
+
+UPDATE animals set owner_id = 4 WHERE name in ('Charmander','Squirtle','Blossom');
+
+UPDATE animals set owner_id = 5 WHERE name in ('Angemon','Boarmon');
        
