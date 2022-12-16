ee38cb10921954fc8195bf9e735a4e94d20d3a4f
OpondoG <okonjogilbert2@gmail.com>
2022-12-16 11:26:46 +0300

    Initial data structure

--- queries.sql
+++ queries.sql
@@ -1,3 +1,18 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+
+SELECT name from animals where date_of_birth BETWEEN'2016-08-11' AND '2019-05-10';
+
+SELECT name from animals where neutered = true AND escape_attempts < 3;
+
+SELECT date_of_birth from animals where name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts from animals where weight_kg > 10.5;
+
+SELECT * from animals WHERE neutered = true;
+
+SELECT * from animals WHERE name != 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
