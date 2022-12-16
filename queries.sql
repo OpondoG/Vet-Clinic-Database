0097fe60f6bc2e250ff47b2729828c71fbad7e89
OpondoG <okonjogilbert2@gmail.com>
2022-12-16 16:51:26 +0300

    Make changes

--- queries.sql
+++ queries.sql
@@ -22,12 +22,12 @@ BEGIN;
 UPDATE animals SET species = 'unspecified';
 ROLLBACK;
 
-BEGIN
+BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 
-BEGIN
+BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
@@ -44,7 +44,7 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
-SELECT MAX(weight_kg), MIN(weight_kg) FROM animals;
-SELECT species, AVG(escape_attempts) FROM animals
+SELECT MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals GROUP BY species
 WHERE date_of_birth BETWEEN '01-01-1990' AND '01-01-2000';
 
